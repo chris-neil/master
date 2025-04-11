a6d013682a21bde4ae4e0e37c104b0e1b0e6aa4d
chris_neil <cneil@eml.cc>
2025-04-11 18:37:38 +0100

    Many changes to Terraform and VMware automation

--- CheckPoint/Python/set_ip_hostname.py
+++ CheckPoint/Python/set_ip_hostname.py
@@ -1,12 +1,22 @@
-from pexpect import pxssh
-s = pxssh.pxssh()
-username = input("Enter username:")
-password = input("Enter password:")
-if not s.login ('192.168.182.250', username, password):
-    print("SSH session failed on login.")
-    print(str(s))
-else:
-    print("SSH session login successful")
-    s.sendline ('set hostname vsx03')
-    s.sendline ('save config')
-    s.logout()
+import paramiko
+import time
+
+ssh=paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+hostname="192.168.182.250"   
+username = "admin" #input("Enter username:")
+password = "Admin123" #input("Enter password:")
+ipaddress="192.168.182.13"
+
+#ssh.connect(hostname=hostname,port=22,username=username,password=password)
+#ssh.exec_command("set interface eth0 ipv4-address " + ipaddress + " mask-length 24")
+#ssh.close
+
+#time.sleep(0.5)
+
+ssh.connect(hostname=ipaddress,port=22,username=username,password=password)
+ssh.exec_command("set hostname vsx03")
+ssh.exec_command("save config")
+ssh.close
+
